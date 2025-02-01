2c478488b0a1891e76352ab2a5d0558c8dbf59a4
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-01 02:10:02 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,15 @@
 // Task 1 - Product Price Managagement Scenario
+
 let prices = [10, 20, 30, 40, 50]; // Declaring an array prices.
 prices.push(60); // Adding a new price to array.
 prices.shift(); // Removing the first price from array.
-console.log("Updated Price List", prices); // Logging updated price list to console.
+console.log("Updated Price List:", prices); // Logging updated price list to console.
+
+
+// Task 2 - Modifying Customer Orders Scenario
+
+let orders = [12, 13, 14, 15, 16]; // Declaring an array orders.
+orders[2] += 5; // Increasing third order by 5.
+let totalNumberofOrders = orders.reduce((total, quantity) => total + quantity, 0); // Calculating total numbers of orders.
+console.log("Updated Order List:", orders);
+console.log("Total Number of Orders:", totalNumberofOrders); // Logging updated array and total order count to console.
