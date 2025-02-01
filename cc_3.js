de72db0b963fbec0e45d897793dbe8383f8d08b7
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-01 01:50:29 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1 - Product Price Managagement Scenario
+let prices = [10, 20, 30, 40, 50]; // Declaring an array prices.
+prices.push(60); // Adding a new price to array.
+prices.shift(); // Removing the first price from array.
+console.log("Updated Price List", prices); // Logging updated price list to console.
