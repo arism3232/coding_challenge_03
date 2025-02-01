73752d437ba2857f4494165f4ed648d1a5dd927e
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-01 04:23:35 -0500

    Task 5 - Inventory Management System

--- cc_3.js
+++ cc_3.js
@@ -28,7 +28,7 @@ let employee = {
 employee.performanceScore = 85; // Updating the performanceScore property.
 employee.promotionEligible = true; 
 if (employee.performanceScore <= 90) {
-    employee.promotionEligible = false
+    employee.promotionEligible = false;
 }; // Adding new property promotionEligible with boolean value.
 console.log("Updated Employee Performance Tracking: ", employee); // Logging updated employee object to console.
 
@@ -45,3 +45,18 @@ feedback.push(
     {customerName: "Shin Yuna", feedbackText: "They never have the things I need.", rating: 3}
 ); // Adding new feedback object to array.
 console.log("Customer Feedback Records: ", feedback); // Logging entire feedback list to console. 
+
+
+
+// Task 5 - Inventory Management System Scenerio
+
+let inventory = {
+    itemName: "Perfume",
+    stockCount: 15,
+    price: 40,
+}; // Declaring object inventory with properties. 
+inventory.calculateTotalValue = function() {
+    return this.stockCount * this.price;
+}; // Adding method calculateTotalValue that returns total value.
+console.log("Inventory Details: ", inventory); 
+console.log("Total Value: ", inventory.calculateTotalValue()); // Logging inventory details and total value to console.
